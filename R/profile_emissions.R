e13151f849cebcb60cf4bdfbb35c160dab413f85
oSamTo <samtom@ceh.ac.uk>
2021-11-24 14:21:52 +0000

    minor update

--- R/profile_emissions.R
+++ R/profile_emissions.R
@@ -14,6 +14,7 @@ y_emis <- 2019 # the emissions year
 ## !! Need a way of choosing NFR/fuel combos to be modelled on specific year data, as opposed to means !! ##
 y_spec_NFR <- NULL # the NFR codes that are to have year-specific profiles (otherwise `average` year = 0)
 species <- "NOx"
+classification <- "SNAP"
 
 ## read and match the NAEI data to the temporal profile in the lookup table, along with SNAP sector
 dt_naei_profs <- JoinNAEItoProfiles(year = y_emis, species = species) 
@@ -22,9 +23,9 @@ dt_naei_profs <- JoinNAEItoProfiles(year = y_emis, species = species)
 # choose either integers for SNAP, or capital letters for GNFR
 v_sectors <- list(1,2,3,4,5,6,7,8,9,10,11)
 
-l_DUKEMs_profiles <- lapply(X=setNames(v_sectors, v_sectors), FUN = EmissionsProfileBySector, year = y_emis, species = species, classification = "SNAP", emis = dt_naei_profs, yr_spec_NFR=NULL, hod_by_dow=F, hour_emis=T)
+l_DUKEMs_profiles <- lapply(X=setNames(v_sectors, v_sectors), FUN = EmissionsProfileBySector, year = y_emis, species = species, classification = classification, emis = dt_naei_profs, yr_spec_NFR=NULL, hod_by_dow=F, hour_emis=T)
 
-saveRDS(l_DUKEMs_profiles, paste0("C:/FastProcessingSam/DUKEMS/totals_profiled/",species,"_SNAP_",y_emis,"_tp.rds"))
+saveRDS(l_DUKEMs_profiles, paste0("./doc/",species,"_",classification,"_",y_emis,"_TP.rds"))
 
 ####################################################################################################
 ####################################################################################################
